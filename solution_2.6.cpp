6621829c16539b8c53fee18df3ba06ccd13b66da
Simon Racz <simonracz@gmail.com>
2015-09-06 15:50:15 +0200

    2.6

---
+++ solution_2.6.cpp
@@ -0,0 +1,7 @@
+//two iterators, it1, it2
+//it 2 runs twice as fast
+//when they collide, both are in the loop - O(n)
+//calculate loop's size L - O(n)
+//it1 start from the beginng
+//it2 start from it1 + L
+//run them at the same speed, when they collide, that's the loop start P
